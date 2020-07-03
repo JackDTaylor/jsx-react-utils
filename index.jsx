d228ef4c663ad47803d510f9ac9139616bce1682
Jack D. Taylor <msec.nt@gmail.com>
2020-07-03 12:07:56 +0300

    Add ability to omit initializer params

--- index.jsx
+++ index.jsx
@@ -3,16 +3,19 @@ import JsxReactUtils from "./base/JsxReactUtils";
 import common from "./common";
 import frontend from "./frontend"
 
-export default function importJsxReactUtils(modules = ['common', 'frontend'], config = {}) {
+export default function importJsxReactUtils(modules = null, config = {}) {
 	const availableModules = {common, frontend};
+
+	if(arguments.length < 2 && modules instanceof Array == false) {
+		config = modules || {};
+		modules = Object.keys(availableModules);
+	}
 
 	for(const module of modules) {
 		if(module in availableModules) {
 			availableModules[module](config);
 		}
 	}
-
-
 
 	return JsxReactUtils.init();
 }
